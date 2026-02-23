2364b2a874b2633444f2c51334731c7d2fbdd5b4
Emrehrz <horuzemre3@gmail.com>
2026-02-23 17:38:43 +0300

    favicon & metadate update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,25 +5,48 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Royal Motors | Gaziantep Şahinbey Oto Elektrik ve Klima Servisi',
+  title: 'Royal Motors | Gaziantep Şahinbey Oto Tamir ve Elektrik Servisi',
   description:
-    'Mavikent Sanayi Sitesi Oto Elektrik ve Klima Servisi - Royal Motors. Gaziantep Şahinbey araç tamir, bakım, klima gazı ve oto elektrik çözümlerinde güvenilir adresiniz.',
+    'Mavikent Sanayi Sitesi Oto Tamir ve Elektrik Servisi - Royal Motors. Gaziantep Şahinbey araç tamir, bakım, klima gazı ve oto elektrik çözümlerinde güvenilir adresiniz.',
   keywords: [
     'Gaziantep Şahinbey Oto Tamir',
     'Mavikent Sanayi Sitesi Oto Elektrik',
     'Klima Servisi',
     'Royal Motors',
     'Gaziantep Araç Tamir',
+    'Araç Arıza',
+    'Motor Tamir'
   ],
   openGraph: {
     type: 'website',
     locale: 'tr_TR',
-    url: 'https://royalmotorsgaziantep.com',
+    url: 'https://www.aliciototamir.com/',
     title: 'Royal Motors | Gaziantep Şahinbey Oto Elektrik ve Klima Servisi',
     description:
       'Mavikent Sanayi Sitesi Oto Elektrik ve Klima Servisi - Royal Motors. Gaziantep Şahinbey bölgesinde uzman hizmet.',
     siteName: 'Royal Motors',
   },
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon/favicon.ico' },
+    ],
+    apple: [
+      { url: '/favicon/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/favicon/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/favicon/android-chrome-512x512.png',
+      },
+    ],
+  },
+  manifest: '/favicon/site.webmanifest',
 };
 
 export default function RootLayout({
